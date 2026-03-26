03a0fcf8ff7b9a6318e29c6c53536711116207fb
Emmanuel Christian <christianemmanuelchidubem@gmail.com>
2026-03-26 11:16:49 -0400

    Track backend as normal folder

---
+++ backend/mysql.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = pool.promise();
